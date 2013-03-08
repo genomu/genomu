42c00286982080ae29c70de11cbd52873cbc994f
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-08 13:02:51 -0800

    Basic channel/transaction functionality testing

---
+++ apps/genomu/test/channel_test.exs
@@ -0,0 +1,66 @@
+Code.require_file "../test_helper.exs", __FILE__
+
+defmodule Genomu.ChannelTest do
+  use Genomu.TestCase
+
+  alias Genomu.Client, as: C
+  alias Genomu.Client.Channel, as: Ch
+  alias Genomu.API, as: API
+
+  setup_all do
+    start_db
+    {:ok, [conn: connection]}
+  end
+
+  teardown_all do
+    stop_db
+  end
+
+  test "channel retrieving a key that has no value", context do
+    conn = context[:conn]
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.get(ch, ["unset"], API.Core.identity) == nil
+  end
+
+  test "channel retrieving a key that was committed by another channel", context do
+    conn = context[:conn]
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.set(ch, ["unused"], API.Core.identity("123")) == "123"
+    C.commit(ch)
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.get(ch, ["unused"], API.Core.identity) == "123"
+  end
+
+  test "channel retrieving a key that was set by the same channel", context do
+    conn = context[:conn]
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.set(ch, ["another"], API.Core.identity("123")) == "123"
+    C.commit(ch)
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.set(ch, ["another"], API.Core.identity("321")) == "321"
+    assert Ch.get(ch, ["another"], API.Core.identity) == "321"
+  end
+
+  test "concurrently committing channels", context do
+    conn = context[:conn]
+
+    {:ok, ch} = C.begin(conn)
+    {:ok, ch1} = C.begin(conn)
+
+    Ch.set(ch, ["ctr"], API.Number.incr)
+    Ch.set(ch, ["ctr"], API.Number.incr)
+    Ch.set(ch1, ["ctr"], API.Number.incr)
+
+    C.commit(ch1)
+    C.commit(ch)
+
+    {:ok, ch} = C.begin(conn)
+    assert Ch.get(ch, ["ctr"], API.Core.identity) == 3
+  end
+
+end
