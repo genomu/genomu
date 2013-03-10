c3ef6a016a434ed535ce49145d3c1284aedc771e
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-10 03:08:57 -0700

    Make idling visually recognizable

--- apps/genomu/priv/static/js/genomu.js
+++ apps/genomu/priv/static/js/genomu.js
@@ -1,11 +1,15 @@
 var app = angular.module('genomuApp', ['ngResource','ui']);
 var idle = false;
+var idleNoty = null;
 
 $(document).idle({
   onIdle: function(){
+    window.idleNoty = $.noty({modal: true, modal: true, timeout: false,
+                              text: 'After 10 seconds of inactivity, pausing data polling...'});
     window.idle = true;
   },
   onActive: function(){
+    $.noty.close(window.idleNoty);
     window.idle = false;
   },
   idle: 10000
