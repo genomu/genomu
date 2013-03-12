19c3bbeb7e047e3f35a6b5342df9f728e052de4e
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-11 17:03:09 -0700

    In test instances, use detected hostname as a host name for the URL

--- test/config.2.exs
+++ test/config.2.exs
@@ -1,6 +1,7 @@
 Genomu.Config.config do
   {:ok, hostname} = :inet.gethostname
   config.instance_name (hostname |> to_binary) <> "2"
+  config.hostname (hostname |> to_binary)
   config.http_port 9129
   config.handoff_port 8100
   config.quiet_mode true
