e28e6910ce2aae8c611141bd26434e0515806c0e
Yurii Rashkovskii <yrashk@spawngrid.com>
2013-03-16 10:58:48 -0700

    Replace :erlang.now with :os.timestamp

--- apps/genomu/lib/utils.ex
+++ apps/genomu/lib/utils.ex
@@ -1,12 +1,12 @@
 defmodule Genomu.Utils do
 
   @doc """
-  Returns current time (:erlang.now) in microseconds
+  Returns current time in microseconds
   """
   @spec now_in_microseconds :: non_neg_integer
 
   def now_in_microseconds do
-    {macro, sec, micro} = :erlang.now
+    {macro, sec, micro} = :os.timestamp
     macro*1000000000000+sec*1000000+micro
   end
 
